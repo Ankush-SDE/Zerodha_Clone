1d130d8630415b0ebd482a01aec6b221621a1618
Ankush Kumar <ankush1524kumar@gmail.com>
2026-07-23 20:51:17 +0530

    structure component

---
+++ frontend/src/landing_page/home/Education.js
@@ -0,0 +1,9 @@
+import React, { useInsertionEffect } from 'react';
+function Education() {
+  return (  
+    <h1>Education</h1>
+
+  );
+}
+
+export default Education;
---
+++ frontend/src/landing_page/openAccount.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function openAccount() {
+  return ( 
+    <h1>openAccount</h1>
+   );
+}
+
+export default openAccount;
